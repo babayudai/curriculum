36e7b5beb19dda8415f9fc2b18a43591d740dfde
“yitest00002” <yitest00002@gmail.com>
2021-08-15 11:22:43 +0900

    1-4
    課題提出テスト

---
+++ JS/1-4/main.js
@@ -0,0 +1,14 @@
+for(let number = 1;number <= 100;number++){
+    if(number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz!");
+        continue;
+    }else if(number % 3 === 0){
+        console.log("Fizz!");
+    }else if(number % 5 === 0){
+        console.log("Buzz!");
+    }else if(number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz!");
+    }else{
+        console.log(number);
+    }
+}
